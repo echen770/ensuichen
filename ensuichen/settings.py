871e6de55611102dcd4a9f5d9bef7ba2b2203f5a
Ensui Chen <echen770@gmail.com>
2021-09-16 12:11:06 -0700

    create base template

--- ensuichen/settings.py
+++ ensuichen/settings.py
@@ -12,15 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .secrets import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uf8$cz^tiu+5@)!3qfv_e&n2b12m_8q#=_a!i^yq4@dv+jvg*1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home/home.html")
+
+
+def projects(request):
+    return render(request, "home/projects.html")
